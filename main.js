448d5df446d627995514be12fa3c1bdb2ac8eb20
David Orpen <d.l.orpen@gmail.com>
2021-06-05 21:41:16 +0200

    Electron forge config sort of starting to work

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+  });
 
-  win.loadFile('index.html')
+  win.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
