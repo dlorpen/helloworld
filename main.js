c5738bfd630063f9cbdd61b57181c17aa4e9219f
David Orpen <d.l.orpen@gmail.com>
2021-06-06 08:47:20 +0200

    Ignoring default menu

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
+
+if (require("electron-squirrel-startup")) return app.quit();
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,6 +12,7 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
+  Menu.setApplicationMenu(null);
   createWindow();
 
   app.on("activate", function () {
