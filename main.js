dd2df907c50cb2ed5484332399d8f6f9cb8512fd
David Orpen <d.l.orpen@gmail.com>
2021-06-07 21:33:11 +0200

    Not really making progress here

--- main.js
+++ main.js
@@ -5,92 +5,87 @@ if (require("electron-squirrel-startup")) return app.quit();
 let win = null;
 
 function createWindow() {
-    const oldWin = win;
-    
-    win = new BrowserWindow({
-	width: 800,
-	height: 600,
-    });
+  const oldWin = win;
 
-    win.loadFile("index.html");
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
-    oldWin?.destroy();
+  win.loadFile("index.html");
+
+  oldWin?.destroy();
 }
 
 const openFile = async () => {
-    try {
-	const { filePaths, canceled }
-	      = await dialog.showOpenDialog(
-		  { properties: ["openFile"] });
-	if (canceled)
-	    return;
-	const fileText
-	      = await readFile(filePaths[0], 'utf8');
-	win.currentFilePath = filePaths[0];
-	console.log(fileText);
-    } catch (err) {
-	console.error(err);
-    }
+  try {
+    const { filePaths, canceled } = await dialog.showOpenDialog({
+      properties: ["openFile"],
+    });
+    if (canceled) return;
+    const fileText = await readFile(filePaths[0], "utf8");
+    win.currentFilePath = filePaths[0];
+    console.log(fileText);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const saveFile = async () => {
-    if (!win?.currentFilePath)
-	saveFileAs();
-    else
-	console.log(`Saving to file: ${win.currentFilePath}`);
+  if (!win?.currentFilePath) saveFileAs();
+  else console.log(`Saving to file: ${win.currentFilePath}`);
 };
 
 const saveFileAs = async () => {
-    try {
-	const { filePath, canceled }
-	      = await dialog.showSaveDialog(
-		  { properties: ["createDirectory"] });
-	if (canceled)
-	    return;
-	win.currentFilePath = filePath;
-	saveFile();
-    } catch (err) {
-	console.error(err);
-    }
+  try {
+    const { filePath, canceled } = await dialog.showSaveDialog({
+      properties: ["createDirectory"],
+    });
+    if (canceled) return;
+    win.currentFilePath = filePath;
+    saveFile();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const showHelpAbout = () => {
-    dialog.showMessageBox(null, {
-	message: "HelloWorld",
-	type: "info",
-	buttons: ["OK"],
-    });
+  dialog.showMessageBox(null, {
+    message: "HelloWorld",
+    type: "info",
+    buttons: ["OK"],
+  });
 };
 
 app.whenReady().then(() => {
-    const isMac = process.platform === "darwin";
-    const template = [
-	{
-	    label: "&File",
-	    submenu: [
-		{ label: "&New", click: async () => createWindow() },
-		{ label: "&Open", click: () => openFile() },
-		{ label: "&Save", click: () => saveFile() },
-		{ label: "Save &As", click: () => saveFileAs() },
-		{ type: "separator" },
-		isMac ? { role: "close" } : { role: "quit" },
-	    ],
-	},
-	{
-	    label: "&Help",
-	    submenu: [{ label: "About", click: () => showHelpAbout() }],
-	},
-    ];
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+  const isMac = process.platform === "darwin";
+  const template = [
+    {
+      label: "&File",
+      submenu: [
+        { label: "&New", click: async () => createWindow() },
+        { label: "&Open", click: () => openFile() },
+        { label: "&Save", click: () => saveFile() },
+        { label: "Save &As", click: () => saveFileAs() },
+        { type: "separator" },
+        isMac ? { role: "close" } : { role: "quit" },
+      ],
+    },
+    {
+      label: "&Help",
+      submenu: [{ label: "About", click: () => showHelpAbout() }],
+    },
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
-    createWindow();
+  createWindow();
 
-    app.on("activate", function () {
-	if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") app.quit();
 });
